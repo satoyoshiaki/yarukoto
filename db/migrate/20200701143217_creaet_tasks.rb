bc8df37b73ebb1615d35d929f10324cc21485ce0
佐藤良昭 <yoshiprograming@gmail.com>
2020-07-02 10:55:59 +0900

    taskモデルの作成

--- db/migrate/20200701143217_creaet_tasks.rb
+++ db/migrate/20200701143217_creaet_tasks.rb
@@ -1,8 +1,10 @@
 class CreaetTasks < ActiveRecord::Migration[5.2]
   def change
-    t.string :yarukoto
-    t.string :rink
-    t.text :comment
-    t.string :image
+    create_table :tasks do |t|
+      t.string :yarukoto
+      t.string :rink
+      t.text :comment
+      t.string :image
+    end
   end
 end
